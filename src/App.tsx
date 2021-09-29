a6258ef3b6b7279651d4f529d5f54c34fd045863
kenta0313 <asanokenta@me.com>
2021-09-29 12:06:49 +0900

    ✨ signup function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Amplify, { Auth } from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
+import {
+  AuthState,
+  onAuthUIStateChange,
+  CognitoUserInterface,
+} from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
+import { RecoilRoot } from "recoil";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Amplify.configure(awsconfig);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    toolBar: {
+      display: "flex",
+    },
+    signOut: {
+      marginLeft: "auto",
+      display: "flex",
+    },
+  })
+);
+
+const App = () => {
+  const classes = useStyles();
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<CognitoUserInterface | undefined>();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState as AuthState);
+      setUser(authData as CognitoUserInterface);
+    });
+  }, []);
+
+  const handleClick = () => {
+    Auth.signOut();
+  };
+
+  return authState === AuthState.SignedIn && user ? (
+    <div>
+      <RecoilRoot>
+        <AppBar className={classes.appBar}>
+          <Toolbar className={classes.toolBar}>
+            <Typography variant="h6" noWrap>
+              ChatApp
+            </Typography>
+            <div onClick={handleClick} className={classes.signOut}>
+              <IconButton
+                aria-label="display more actions"
+                edge="end"
+                color="inherit"
+              >
+                <ExitToAppIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </RecoilRoot>
     </div>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+        ]}
+      />
+    </AmplifyAuthenticator>
   );
-}
+};
 
 export default App;
